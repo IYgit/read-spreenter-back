ea6916cd64cb9d72526f4326931a8dffab87acc7
IYgit <novemb75@gmail.com>
2026-03-08 15:40:50 +0200

    start project

---
+++ src/main/java/com/iyanc/javarush/readsprinterback/ReadSprinterBackApplication.java
@@ -0,0 +1,13 @@
+package com.iyanc.javarush.readsprinterback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadSprinterBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadSprinterBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iyanc/javarush/readsprinterback/ReadSprinterBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iyanc.javarush.readsprinterback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadSprinterBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
